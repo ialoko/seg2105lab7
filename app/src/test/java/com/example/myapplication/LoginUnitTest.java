7599df6461e5064b893678ebc5913bf353bd253e
Iyiola <48067557+ialoko@users.noreply.github.com>
2021-11-07 21:30:17 -0500

    push changes

--- app/src/test/java/com/example/myapplication/LoginUnitTest.java
+++ app/src/test/java/com/example/myapplication/LoginUnitTest.java
@@ -34,13 +34,15 @@ public class LoginUnitTest {
     @Test
     public void testIsEmail(){ //testing if is email with valid input
         LoginActivity myObjectUnderTest = new LoginActivity(mMockContext);
-        String result = myObjectUnderTest.
+        Boolean result = myObjectUnderTest.isEmail("test@gmail.com");
+        assertEquals(result, true);
 
     }
 
     @Test
     public void testIsNotEmail(){ //testing is email with invalid input
         LoginActivity myObjectUnderTest = new LoginActivity(mMockContext);
-
+        Boolean result = myObjectUnderTest.isEmail("test");
+        assertEquals(result, false);
     }
 }
